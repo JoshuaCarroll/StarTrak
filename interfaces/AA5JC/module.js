afc61abacac2163b7e0a260ecf9c6ed775f54453
Joshua Carroll <joshcarroll21@gmail.com>
2020-07-23 16:18:17 -0500

    Changed links

--- interfaces/AA5JC/module.js
+++ interfaces/AA5JC/module.js
@@ -203,8 +203,8 @@ function buildNemesisUi() {
 								{type:'wrapper', flex:'h', class:'button-wrap headerButtons', children:[
 									{type:'button', color:LCARS.colorGen(uiColors), version:'left', label:'Full screen', href:'javascript:toggleFullScreen();'},
 									{type:'button', color:LCARS.colorGen(uiColors), version:'button', label:'Get data', href:'javascript:getData();'},
-									{type:'button', color:LCARS.colorGen(uiColors), version:'left', label:'49960', href:'javascript:setContent("https://aa5jc.ngrok.io/supermon/link.php?nodes=49960")'},
-									{type:'button', color:LCARS.colorGen(uiColors), version:'button', label:'1999', href: 'javascript:setContent("https://aa5jc.ngrok.io/supermon/link.php?nodes=1999")'}
+									{type:'button', color:LCARS.colorGen(uiInactive), version:'left', label:'', href:''},
+									{type:'button', color:LCARS.colorGen(uiColors), version:'button', label:'Digital Bridge', href: 'javascript:setContent("http://digitalbridge.southcentralus.cloudapp.azure.com/allmon2/link.php?nodes=499601")'}
 								]}
 							]}	
 						]}
