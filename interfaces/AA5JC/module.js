b7c2c4be72a8f7bf0718c573311c664bd3f89377
Joshua Carroll <joshcarroll21@gmail.com>
2020-07-23 21:05:22 -0500

    Changed a thing

--- interfaces/AA5JC/module.js
+++ interfaces/AA5JC/module.js
@@ -203,8 +203,8 @@ function buildNemesisUi() {
 								{type:'wrapper', flex:'h', class:'button-wrap headerButtons', children:[
 									{type:'button', color:LCARS.colorGen(uiColors), version:'left', label:'Full screen', href:'javascript:toggleFullScreen();'},
 									{type:'button', color:LCARS.colorGen(uiColors), version:'button', label:'Get data', href:'javascript:getData();'},
-									{type:'button', color:LCARS.colorGen(uiInactive), version:'left', label:'', href:''},
-									{type:'button', color:LCARS.colorGen(uiColors), version:'button', label:'Digital Bridge', href: 'javascript:setContent("https://digitalbridge.southcentralus.cloudapp.azure.com/allmon2/link.php?nodes=499601")'}
+									{type:'button', color:LCARS.colorGen(uiInactive), version:'left', label:'' },
+									{type:'button', color:LCARS.colorGen(uiColorsDark), version:'button', label:'Digital Bridge', href: 'javascript:setContent("https://digitalbridge.southcentralus.cloudapp.azure.com/allmon2/link.php?nodes=499601")'}
 								]}
 							]}	
 						]}
